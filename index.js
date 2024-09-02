65731a8bd9b4d17deaab80f00cba0f861285bced
Laiba Muhammad Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-09-02 21:26:57 +0500

    total tasks functionality

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ let addItemInput = document.getElementById("addItemInput");
 let addbtn = document.getElementById("addbtn");
 let tasklist = document.getElementById("task-list");
 
+
+// let counter = 0;
+let totalTasks = 0;
+let completedTasks = 0;
+
+
 let currentDate = new Date();
 let months = ["January", "February", "March", "April", "May", "June", 
               "July", "August", "September", "October", "November", "December"];
@@ -25,6 +31,13 @@ date.innerText = `${currentDate.getDate()} ${month} ${currentDate.getFullYear()}
 addbtn.addEventListener("click", function() {
     let item = addItemInput.value;
     addItem(item);
+    // counter++;
+    // totalTasks = totalTasks + counter;
+    // console.log(counter)
+    totalTasks++;
+    console.log("Total tasks " + totalTasks)
+    // console.log("Total tasks" + totalTasks);
+
 });
 
 function addItem(item) {
@@ -45,32 +58,52 @@ function addItem(item) {
     let checkBox = task.querySelector(".checkbox");
     let taskName = task.querySelector(".taskName");
 
+    // let completedTasks = 0;
+
     checkBox.addEventListener("change", function() {
         if(checkBox.checked) {
             taskName.style.textDecoration = "line-through";
+            completedTasks++;
+            console.log(completedTasks)
         } else {
             taskName.style.textDecoration = "none";
         }
+
+        let remainingTasks = totalTasks - completedTasks;
+        // var remainingTasks = totalTasks - completedTasks;
+        console.log("Remainig Tasks " + remainingTasks);
     });
 
 }
 }
 
-function edit(event){
-    let changeDisabled = event.target.parentNode.childNodes[2].disabled;
-    changeDisabled = false;
-    console.log(changeDisabled);
-    if(changeDisabled === false){
-        console.log(event.target.parentNode.childNodes[2].value);
-        // taskName.focus();
-        event.target.parentNode.childNodes[2].focus();
-    }
-    // console.log(event.target.parentNode.childNodes[2].disabled = true);
+// let remainingTasks = totalTasks - completedTasks;
+// console.log("Remainig Tasks " + remainingTasks);
+
+// function edit(event){
+//     let changeDisabled = event.target.parentNode.childNodes[2].disabled;
+//     changeDisabled = false;
+//     console.log(changeDisabled);
+//     if(changeDisabled === false){
+//         console.log(event.target.parentNode.childNodes[2].value);
+//         // taskName.focus();
+//         event.target.parentNode.childNodes[2].focus();
+//     }
+//     // console.log(event.target.parentNode.childNodes[2].disabled = true);
+// }
+
+function edit(event) {
+    let taskName = event.target.parentNode.childNodes[2];
+    taskName.disabled = false; 
+    taskName.focus();
 }
+
 
 function deleteList(event){
     console.log(event.target.parentNode);
     event.target.parentNode.remove();
+    // remainingTasks--;
+    // console.log("after delete " + remainingTasks)
 }
 
 let deleteAll = document.getElementById("deleteAll");
